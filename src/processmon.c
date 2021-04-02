e2619b77de61616ddb0bd894841200f8f59ddd94
wangxiaofan <paradoxes@163.com>
2021-04-02 17:41:13 +0800

    na

--- src/processmon.c
+++ src/processmon.c
@@ -14,6 +14,10 @@ static int is_int(const char *str)
     int len = strlen(str);
     int i = 0;
 
+    if(!str || !len){
+        return 0;
+    }
+
     for (i = 0; i < len; i++){
         if (!(isdigit(str[i]))){
             return 0;
@@ -23,20 +27,18 @@ static int is_int(const char *str)
     return 1;
 }
 
-static void trave_dir(char *path)
+static void trave_dir(PROC_FUNC func)
 {
     DIR *d = NULL;
     struct dirent *dp = NULL;
     struct stat st;
     char p[2048] = {0};
-    
-    if(stat(path, &st) < 0 || !S_ISDIR(st.st_mode)) {
-        printf("invalid path: %s\n", path);
-        return;
-    }
+    char buff[4097] = {0};
+    int fd, ret;
+    char pid[2048] = {0};
 
-    if(!(d = opendir(path))) {
-        printf("opendir[%s] error: %m\n", path);
+    if(!(d = opendir(PROC_PATH))) {
+        printf("opendir %s error.\n", PROC_PATH);
         return;
     }
 
@@ -44,15 +46,38 @@ static void trave_dir(char *path)
         if((!strncmp(dp->d_name, ".", 1)) || (!strncmp(dp->d_name, "..", 2))){
             continue;
         }
-        snprintf(p, sizeof(p) - 1, "%s/%s", path, dp->d_name);
-        stat(p, &st);
+        snprintf(p, sizeof(p) - 1, "%s/%s", PROC_PATH, dp->d_name);
+        if(stat(p, &st) < 0){
+            printf("stat [%s] err.\n", p);
+            continue;
+        }
         if(!S_ISDIR(st.st_mode)) {
             continue;
         }
         if(!is_int(dp->d_name)){
             continue;
         }
-        printf("DIR: %s\n", p);
+        if(!strcmp(dp->d_name, "1")){
+            continue;
+        }
+        strcpy(pid, dp->d_name);
+
+        snprintf(buff, sizeof(buff) - 1, "%s/cmdline", p);
+
+        fd = open(buff, O_RDONLY);
+        if(fd < 0){
+            printf("open [%s] err.\n", buff);
+            continue;
+        }
+        memset(buff, 0, sizeof(buff));
+        ret = read(fd, buff, sizeof(buff) - 1);
+        close(fd);
+
+        if(ret < 1){
+            continue;
+        }
+
+        func(pid, buff, ret);
     }
 
     closedir(d);
@@ -60,9 +85,18 @@ static void trave_dir(char *path)
     return;
 }
 
+static int show(const char *pid, char *cmdline, int reallen)
+{
+    printf("%-10s %-10d %s\n", pid, reallen, cmdline);
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
-    trave_dir("/proc");
+    (void)argc;
+    (void)argv;
+
+    trave_dir(&show);
 
     return 0;
 }
--- src/processmon.h
+++ src/processmon.h
@@ -1,5 +1,9 @@
 #ifndef _PROCESSMON_H
 #define _PROCESSMON_H
 
+#define PROC_PATH "/proc"
+
+typedef int (*PROC_FUNC)(const char *pid, char *cmdline, int reallen);
+
 #endif
 
